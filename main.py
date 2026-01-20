4202d45c6268ba3bc7643290318d32a852bd5bd7
hermioneleroy <hermioneleroy05@gmail.com>
2026-01-20 11:55:44 +0000

    test sans permutations..?

--- main.py
+++ main.py
@@ -140,87 +140,6 @@ def is_time_possible(row_f, sequence, instance_idx):
         prev_order = j
     return True
 
-"""
-### GESTION DES CONTRAINTES
-def is_route_possible(family, sequence, instance_idx):
-    "
-    Determines if a given sequence of nodes can form a valid route
-    for the specified instance index and family vehicle.
-    
-    Args:
-        family (str): The family of vehicles
-        sequence (list): A list of node indices representing the route.
-        instance_idx (int): The index of the instance file.
-    "
-    if not isinstance(sequence, (list, tuple)):
-        #print("sequence must be a list or tuple.")
-        return False
-    if not (0 <= instance_idx < len(instances)):
-        #print("instance_idx out of range.")
-        return False
-
-    # Pas de dépôt dans la séquence (implicite au début)
-    if 0 in sequence:
-        #print("depot in sequence")
-        return False
-    
-    #Noeuds valides
-    inst_size = len(instances[instance_idx])
-    for node in sequence:
-        if not (0 <= node < inst_size):
-            #print("invalid node in sequence")
-            return False
-    
-    # Pas de doublons dans la séquence
-    if len(sequence) != len(set(sequence)):
-        #print("duplicates in sequence")
-        return False
-    
-    inst = instances[instance_idx]
-    row_f = data_vehicles.loc[data_vehicles["family"] == family].iloc[0]
-    capacity = row_f["max_capacity"]  
-
-    # Capacitée du véhicule respéctée
-    total_weight = 0.0
-    for node in sequence:
-        w = inst[node].get("order_weight", None)
-        if w is None or (isinstance(w, float) and np.isnan(w)):
-            return False
-        total_weight += w
-
-    if total_weight > capacity:
-        #print("capacity exceeded")
-        return False
-    
-    # Contraine de temps ( fenêtres de temps )
-    
-    current_time = 0.0    # temps actuel = départ dépôt
-    prev = 0   # dépôt implicite
-
-    for j in sequence: 
-        i = prev
-        # temps de trajet majoré
-        travel = temps_max(i, j, family, instance_idx)
-        # arrivée brute à j
-        arrival = current_time + travel
-
-        tmin_j = inst[j]["window_start"]
-        tmax_j = inst[j]["window_end"]
-        lj = inst[j]["delivery_duration"]
-
-        # attente autorisée
-        start_service = max(arrival, tmin_j)
-
-        # faisabilité fenêtre
-        if start_service > tmax_j:
-            #print("time window violated")
-            return False
-
-        # départ après service
-        current_time = start_service + lj
-        prev = j
-    return True
-"""
 
 ### OPTIMISATION
 def get_best_vehicle(sequence, instance_idx):
@@ -303,7 +222,7 @@ def eliminer_petites_routes(routes, instance_idx):
         clients_a_deplacer = route_source["sequence"][1:-1]
         
         #que les petites routes
-        if len(clients_a_deplacer) > 7 or len(clients_a_deplacer) == 0:
+        if len(clients_a_deplacer) > 5 or len(clients_a_deplacer) == 0:
             continue
 
         #copies locales pour pouvoir annuler si l'élimination échoue
@@ -415,9 +334,10 @@ for A in range(10):
         family, cost = get_best_vehicle(sequence, A)
 
         if family:
-            optimized_seq = optimize_route_permut(sequence, A, family)
-            final_family, final_cost = get_best_vehicle(optimized_seq, A)
-            final_routes.append({"family":final_family, "sequence":optimized_seq})
+            #optimized_seq = optimize_route_permut(sequence, A, family)
+            #final_family, final_cost = get_best_vehicle(optimized_seq, A)
+            #final_routes.append({"family":final_family, "sequence":optimized_seq})
+            final_routes.append({"family":family, "sequence": sequence})
     
     if final_routes:
         final_routes = eliminer_petites_routes(final_routes, A)
